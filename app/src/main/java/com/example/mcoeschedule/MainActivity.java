526b41e2cb65a21180b4c1fa1f9253beaad6e023
Sushant <sushant_patil_mca@moderncoe.edu.in>
2023-10-15 20:17:56 +0530

    Start

---
+++ app/src/main/java/com/example/mcoeschedule/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.mcoeschedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Main_Screen.class);
+                startActivity(i);
+            }
+
+        });
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu,menu);
+        return true;
+    }
+}
